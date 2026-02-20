65557ac15c76e5551995b6e9e1aba5b99a410929
Sudeep29-code <sudeepkumarsurya@gmail.com>
2026-02-20 17:08:33 +0530

    Final code

--- app.py
+++ app.py
@@ -493,7 +493,7 @@ def get_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="aditya@123",
+        password="sudeep@29",
         database="hospital_db"
     )
 
